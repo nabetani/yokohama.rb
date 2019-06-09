673d7993dcaaaf4fcacce4a6cb91968dd6dc8573
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-06-09 16:55:21 +0900

    上限を 28bit に変更する

--- q/q103/src/problem.rb
+++ q/q103/src/problem.rb
@@ -14,6 +14,8 @@ ALEN_TAIL=ALEN-ALEN_HEAD
 S0="1a"
 S1="aaa"
 S2=0b111110111.to_s(16)
+BITS=28
+
 
 SAMPLES = [
   S0,
@@ -26,18 +28,18 @@ SAMPLES = [
   ),
   [
     14,
-    (0b11<<18)|3,
-    (0b111<<17)|3,
-    (0b111<<17),
-    (0b1111<<16),
-    (0b11111<<15),
-    (1..19).map{ |n|
+    (0b11<<(BITS-2))|3,
+    (0b111<<(BITS-3))|3,
+    (0b111<<(BITS-3)),
+    (0b1111<<(BITS-4)),
+    (0b11111<<(BITS-5)),
+    (1..(BITS-1)).map{ |n|
       e = (("0"*n+"1")*10)
-      e.to_i(2) & 0xfffff
+      e.to_i(2) & ((1<<BITS)-1)
     },
     Array.new(10){ |n|
       rng=Random.new(n)
-      rng.rand(1<<20) & rng.rand(1<<20)
+      rng.rand(1<<BITS) & rng.rand(1<<BITS)
     }
   ].flatten.map{ |e| e.to_s(16) }
 ].flatten.uniq.sort_by{ |x| [ [x,solve(x)].join.size, x ] }
@@ -45,7 +47,7 @@ SAMPLES = [
 def check(s)
   val = s.to_i(16)
   raise s unless val.to_s(16) == s
-  raise s unless 0<=val && Math.log2(val)<=20
+  raise s unless 0<=val && Math.log2(val)<=BITS
 end
 
 SAMPLES.each do |s|
