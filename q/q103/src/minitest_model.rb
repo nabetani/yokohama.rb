9984bc360459b7632ebff39607b244177bd80378
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-06-09 17:15:22 +0900

    minitest を使った実装の雛形を用意する

---
+++ q/q103/src/minitest_model.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require "json"
+
+def solve(src)
+  # write something here.
+end
+
+if ! ARGV[0] || ! File.exist?( ARGV[0] )
+  raise "you should specify json file as ARGV[0]"
+end
+
+class TestYokohamaRb103 < Minitest::Test
+  json_string = File.open( ARGV[0], &:read )
+  data = JSON.parse( json_string, symbolize_names:true )
+  data[:test_data].each do | number:, src:, expected: |
+    define_method( :"test_#{number}" ) do
+      actual = solve(src)
+      assert_equal( expected, actual )
+    end
+  end
+end
