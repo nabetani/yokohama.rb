fe9d0a05a77e3c2e6649360dd2d7ad957f79c04a
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-06-15 12:17:04 +0900

    テストコードを入れた。

--- q/q103/src/solver_test.rb
+++ q/q103/src/solver_test.rb
@@ -3,10 +3,21 @@
 require 'minitest/autorun'
 require_relative "solver"
 
+def solve_slow(src)
+  mask = src.to_i(16)
+  matches = (1..mask).select{ |e| (e & mask)==e }
+  if 15<matches.size
+    [matches[0,13].join(","),matches[-2,2].join(",")].join(",...,")
+  else
+    matches.join(",")
+  end
+end
+
 class SolverTest < Minitest::Test
   def test_samples
-    [
-    ].each do |src, expected|
+    (1..10000).each do |n|
+      src = n.to_s(16)
+      expected = solve_slow(src)
       actual = solve( src )
       assert_equal expected, actual, "src=#{src}"
     end
